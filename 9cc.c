53ed48f303fa7b0b132085610983a841c9333912
Yasuhiro Ohkura <rum.yasuhiro@gmail.com>
2023-01-01 09:37:36 +0900

    追加：単項+、単項-

--- 9cc.c
+++ 9cc.c
@@ -153,6 +153,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr    = mul ("+" mul | "-" mul)*
@@ -169,18 +170,27 @@ Node *expr() {
   }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ("*" unary | "/" unary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     else
       return node;
   }
+}
+
+// unary   = ("+" | "-")? primary
+Node *unary() {
+  if (consume('+'))
+    return unary();
+  if (consume('-'))
+    return new_node(ND_SUB, new_node_num(0), unary());
+  return primary();
 }
 
 // primary = num | "(" expr ")"
